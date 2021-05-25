0423c30ab96bcb4643fd609f104be3ff1039b15f
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-05-25 23:55:52 +0900

    Commit Exp

---
+++ 1_basic_differential/lib/exp.py
@@ -0,0 +1,6 @@
+from function import Function
+import numpy as np
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
---
+++ 1_basic_differential/test/test_exp.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+sys.path.append("../lib")
+from exp import Exp
+from square import Square
+from variable import Variable
+import numpy as np
+
+class TestTemplate(unittest.TestCase):
+    def setUp(self):
+        self.input = np.array(0.5)
+        self.exp = Exp()
+
+    def test_call(self):
+        square_1 = Square()
+        square_2 = Square()
+        x = Variable(self.input)
+        a = square_1(x)
+        b = self.exp(a)
+        y = square_2(b)
+        self.assertEqual(0.25, a.data)
+        self.assertEqual(1.2840254166877414, b.data)
+        self.assertEqual(1.648721270700128, y.data)
+
+    def test_forward(self):
+        y = self.exp.forward(self.input)
+        self.assertEqual(1.6487212707001282, y)
+
+if __name__ == "__main__":
+    unittest.main()
