313337c8d41e80567cfcc8109617d380a29647bd
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-05-25 16:42:35 +0900

    Commit Variable

---
+++ 1_basic_differential/lib/variable.py
@@ -0,0 +1,9 @@
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, new_data):
+        self.data = new_data
---
+++ 1_basic_differential/test/test_variable.py
@@ -0,0 +1,49 @@
+import unittest
+import sys
+sys.path.append("../lib")
+from variable import Variable
+import numpy as np
+from numpy.testing import assert_array_equal
+
+class TestVariable(unittest.TestCase):
+    def test_variable_1(self):
+        data = np.array(1)
+        variable = Variable(data)
+        assert_array_equal(np.array(1), variable.get_data())
+        self.assertEqual(0, variable.get_data().ndim)
+        new_data = np.array([10])
+        variable.set_data(new_data)
+        assert_array_equal(np.array(10), variable.get_data())
+
+    def test_variable_2(self):
+        data = np.array([1, 2, 3])
+        variable = Variable(data)
+        assert_array_equal(np.array([1, 2, 3]), variable.get_data())
+        self.assertEqual(1, variable.get_data().ndim)
+        new_data = np.array([1.0, 2.0, 3.0])
+        variable.set_data(new_data)
+        assert_array_equal(np.array([1., 2., 3.]), variable.get_data())
+
+    def test_variable_3(self):
+        data = np.array([
+            [1, 2, 3],
+            [4, 5, 6]
+        ])
+        variable = Variable(data)
+        assert_array_equal(np.array([
+            [1, 2, 3],
+            [4, 5, 6]
+        ]), variable.get_data())
+        self.assertEqual(2, variable.get_data().ndim)
+        new_data = np.array([
+            [1.0, 2.0, 3.0],
+            [4.0, 5.0, 6.0]
+        ])
+        variable.set_data(new_data)
+        assert_array_equal(np.array([
+            [1., 2., 3.],
+            [4., 5., 6.]
+        ]), variable.get_data())
+
+if __name__ == "__main__":
+    unittest.main()
