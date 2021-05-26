1631639318668caee3507919cdbf050f9224cefd
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-05-26 21:48:08 +0900

    Commit diff

---
+++ 1_basic_differential/lib/diff.py
@@ -0,0 +1,16 @@
+from exp import Exp
+from square import Square
+from variable import Variable
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
---
+++ 1_basic_differential/test/test_diff.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+sys.path.append("../lib")
+from diff import *
+from square import Square
+from variable import Variable
+import numpy as np
+
+class TestDifferenciation(unittest.TestCase):
+    def setUp(self):
+        data   = np.array(0.5)
+        self.x = Variable(data)
+
+    def test_numerical_diff_1(self):
+        dy = numerical_diff(f, self.x)
+        self.assertEqual(3.2974426293330694, dy)
+
+    def test_f(self):
+        dy = f(self.x)
+        self.assertEqual(1.648721270700128, dy.data)
+
+if __name__ == "__main__":
+    unittest.main()
